ab5ba1f54ddd68012c50f654000bdf2fd5f0525b
ueda-shinya <91458249+ueda-shinya@users.noreply.github.com>
2024-01-26 17:56:33 +0900

    24011800

---
+++ js/sp-navmenu.js
@@ -0,0 +1,4 @@
+document.getElementById('y24-sp-menu-btn').addEventListener('click', function() {
+  var element = document.querySelector('.y24-sp-menu-btn_span');
+  element.classList.toggle('y24-sp-menu-open');
+});
