cc38be38ec88d01d54b50bb9b999fa38bbf49fd3
ueda-shinya <91458249+ueda-shinya@users.noreply.github.com>
2024-01-27 18:03:28 +0900

    spスライダー以外終わり

--- js/sp-navmenu.js
+++ js/sp-navmenu.js
@@ -1,4 +1,30 @@
 document.getElementById('y24-sp-menu-btn').addEventListener('click', function() {
-  var element = document.querySelector('.y24-sp-menu-btn_span');
-  element.classList.toggle('y24-sp-menu-open');
+  var element1 = document.querySelector('.y24-sp-menu-btn_span');
+  var element2 = document.querySelector('.y24-sp-menu');
+  
+  // .y24-sp-menu-open クラスをトグルする
+  element1.classList.toggle('y24-sp-menu-open');
+  element2.classList.toggle('y24-sp-menu-open');
+
+  // クラスが存在するかどうかで条件分岐
+  toggleScroll(element1.classList.contains('y24-sp-menu-open'));
 });
+
+// y24-sp-menuの中のaタグにイベントリスナーを設定
+document.querySelectorAll('.y24-sp-menu a').forEach(function(link) {
+  link.addEventListener('click', function() {
+    // y24-sp-menu-open クラスを削除
+    var element1 = document.querySelector('.y24-sp-menu-btn_span');
+    var element2 = document.querySelector('.y24-sp-menu');
+    element1.classList.remove('y24-sp-menu-open');
+    element2.classList.remove('y24-sp-menu-open');
+
+    // スクロールを有効にする
+    toggleScroll(false);
+  });
+});
+
+// スクロールの有効/無効を切り替える関数
+function toggleScroll(isDisabled) {
+  document.body.style.overflow = isDisabled ? 'hidden' : '';
+}
